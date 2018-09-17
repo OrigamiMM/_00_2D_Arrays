342571da8ad206f81d0db7db89ad8ae11be9dee0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-09-17 16:54:12 -0700

    Done with mod 1

---
+++ src/Checkpoint.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Checkpoint {
+	public static void main(String[] args) {
+		int[][] arr = new int[5][5];
+		Random r = new Random();
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				arr[i][j] = r.nextInt(100); 
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j]+ " ");
+			}
+			System.out.println();
+		}
+	}
+}
